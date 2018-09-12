475128483ce7c78325bd8547af905e01e40d71cb
Charles Michael Jr <37586218+charlestmichaeljr@users.noreply.github.com>
2018-09-12 15:32:23 -0400

    Added Pet Type creation to DataLoader

--- pet-clinic-web/src/main/java/com/charlie/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/charlie/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package com.charlie.petclinic.bootstrap;
 
 import com.charlie.petclinic.model.Owner;
+import com.charlie.petclinic.model.PetType;
 import com.charlie.petclinic.model.Vet;
 import com.charlie.petclinic.services.OwnerService;
+import com.charlie.petclinic.services.PetTypeService;
 import com.charlie.petclinic.services.VetService;
-import com.charlie.petclinic.services.map.OwnerServiceMap;
-import com.charlie.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +14,30 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        System.out.println("dog type id " + savedDogPetType.getId());
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("cat type id " + savedCatPetType.getId());
+
+        System.out.println("Loaded Pet Types");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Barack");
