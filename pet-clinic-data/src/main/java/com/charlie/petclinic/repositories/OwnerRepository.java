64bc52cc55efa49c7460c51e50894aadc32fbdbf
Charles Michael Jr <37586218+charlestmichaeljr@users.noreply.github.com>
2018-09-18 11:45:39 -0400

    Added SpringData implementation of OwnerService

--- pet-clinic-data/src/main/java/com/charlie/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/charlie/petclinic/repositories/OwnerRepository.java
@@ -4,6 +4,9 @@ import com.charlie.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner,Integer> {
+    Owner findByLastName(String lastName);
 }
