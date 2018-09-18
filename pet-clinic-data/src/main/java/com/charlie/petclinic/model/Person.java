2f38ceebfa9404e0bcf27ed9ace1ccb0bf405ea0
Charles Michael Jr <37586218+charlestmichaeljr@users.noreply.github.com>
2018-09-18 09:32:09 -0400

    Converted Owner and related types to JPA entities.

--- pet-clinic-data/src/main/java/com/charlie/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/charlie/petclinic/model/Person.java
@@ -1,11 +1,15 @@
 package com.charlie.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
-    private String address;
-    private String city;
-    private String telephone;
+
 
     public String getFirstName() {
         return firstName;
@@ -23,27 +27,4 @@ public class Person extends BaseEntity {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
--- pet-clinic-data/src/main/java/com/charlie/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/charlie/petclinic/model/PetType.java
@@ -1,7 +1,12 @@
 package com.charlie.petclinic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
