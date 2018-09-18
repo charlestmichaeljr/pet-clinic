f1b6a6d050a703bb1cd07b6eb25eb9de5b75abdb
Charles Michael Jr <37586218+charlestmichaeljr@users.noreply.github.com>
2018-09-18 09:09:58 -0400

    Added annotations to BaseEntity class to let JPA know that this is a mapped superclass.

--- pet-clinic-data/src/main/java/com/charlie/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/charlie/petclinic/model/BaseEntity.java
@@ -6,8 +6,11 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     public Integer getId() {
